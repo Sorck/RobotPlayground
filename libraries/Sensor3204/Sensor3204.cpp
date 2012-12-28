2364bbd79a17211c24a23a29ae30405cf73df719
OpenShift guest <git@secondsphere.org>
2012-12-28 12:13:17 -0500

    Coding standards fix

--- libraries/Sensor3204/Sensor3204.cpp
+++ libraries/Sensor3204/Sensor3204.cpp
@@ -18,38 +18,43 @@
 
 
 //------------- ADC READ FUNCTION----------------
-int Sensor3204::read(int channel){
-  int adcvalue = 0;
-  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+int Sensor3204::read(int channel)
+{
+    int adcvalue = 0;
+    //command bits - start, mode, chn (3), dont care (3)
+    byte commandbits = B11000000;
 
-  //allow channel selection
-  commandbits|((channel-1)<<3);
-   //digitalWrite(CS,HIGH);
-  digitalWrite(CS,LOW); //Select adc
-  int i;
-  // setup bits to be written
-  for (i=7; i>=3; i--){
-    digitalWrite(DOUT,commandbits&1<<i);
-    //cycle clock
-    digitalWrite(CLK,HIGH);
-    digitalWrite(CLK,LOW);    
-  }
-
-  digitalWrite(CLK,HIGH);    //ignores 2 null bits
-  digitalWrite(CLK,LOW);
-  digitalWrite(CLK,HIGH);  
-  digitalWrite(CLK,LOW);
-
-  //read bits from adc
-  for (i=11; i>=0; i--){
-    adcvalue+=digitalRead(DIN)<<i;
-    //cycle clock
+    //allow channel selection
+    commandbits|((channel-1)<<3);
+    //digitalWrite(CS,HIGH);
+    digitalWrite(CS,LOW); //Select adc
+    int i;
+    // setup bits to be written
+    for (i=7; i>=3; i--)
+    {
+        digitalWrite(DOUT,commandbits&1<<i);
+        //cycle clock
+        digitalWrite(CLK,HIGH);
+        digitalWrite(CLK,LOW);    
+    }
+    
+    //ignores 2 null bits
     digitalWrite(CLK,HIGH);
     digitalWrite(CLK,LOW);
-  }
-  digitalWrite(CS, HIGH); //turn off device
-  //digitalWrite(CS,LOW);
-  return adcvalue;
+    digitalWrite(CLK,HIGH);  
+    digitalWrite(CLK,LOW);
+    
+    //read bits from adc
+    for (i=11; i>=0; i--)
+    {
+        adcvalue+=digitalRead(DIN)<<i;
+        //cycle clock
+        digitalWrite(CLK,HIGH);
+        digitalWrite(CLK,LOW);
+    }
+    digitalWrite(CS, HIGH); //turn off device
+    //digitalWrite(CS,LOW);
+    return adcvalue;
 }
 
 Sensor3204::Sensor3204()
